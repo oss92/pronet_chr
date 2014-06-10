672f29d81615df81b6e56689399e3c461e50cb46
Mohamed Osama Alnagdy <mohamed.o.alnagdy@gmail.com>
2014-06-10 22:02:22 +0300

    Minor change

--- chr/long_lat_prioritization.pl
+++ chr/long_lat_prioritization.pl
@@ -14,19 +14,10 @@ distanceBetweenGener         @ soi(Street1, Long1, Lat1, Priority1), soi(Street2
 distanceBetween              @ rsu_range(Range, Percentage), soi(Street1, Long1, Lat1, Priority1), soi(Street2, Long2, Lat2, Priority2), distance(Long1, Lat1, Long2, Lat2, _, false) \
                              soi(Street1, Long1, Lat1, Priority1), soi(Street2, Long2, Lat2, Priority2), distance(Long1, Lat1, Long2, Lat2, _, false)  <=>
                              Street1 == Street2,
-                             Long1Rad is Long1 * (22/1260),
-                             Long2Rad is Long2 * (22/1260),
-                             Lat1Rad is Lat1 * (22/1260),
-                             Lat2Rad is Lat2 * (22/1260),
-                             Dlong is Long2Rad - Long1Rad,
-                             Dlat is Lat2Rad - Lat1Rad,
-                             Sin1 is (sin(Dlat/2)) * (sin(Dlat/2)),
-                             Sin2 is cos(Lat1Rad) * cos(Lat2Rad) * ((sin(Dlong/2))*(sin(Dlong/2))),
-                             A is Sin1 + Sin2,
-                             C is 2 * atan2(sqrt(A), sqrt(1-A)),
-                             Distance is 6373.0 * C,
+                             longlat(Long1, Lat1, Long2, Lat2, Distance),
                              Distance =< Range,
-                             Distance >= ((100-(Percentage/2))*(Range/100)) | distance(Long1, Lat1, Long2, Lat2, Distance, true),
+                             Distance >= ((100-(Percentage/2))*(Range/100)) |
+                             distance(Long1, Lat1, Long2, Lat2, Distance, true),
                              soi(Street1, Long1, Lat1, Priority1 + Distance),
                              soi(Street2, Long2, Lat2, Priority2 + Distance).
                              
@@ -41,4 +32,4 @@ longlat(Long1, Lat1, Long2, Lat2, Distance):-
                              Sin2 is cos(Lat1Rad) * cos(Lat2Rad) * ((sin(Dlong/2))*(sin(Dlong/2))),
                              A is Sin1 + Sin2,
                              C is 2 * atan2(sqrt(A), sqrt(1-A)),
-                             Distance is 6373.0 * C.
+                             Distance is 6353000.0 * C.
